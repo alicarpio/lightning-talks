045625edc5b22c94855ec4d8be02664f8e0c4db8
alicarpio <agcarpio@espol.edu.ec>
2023-03-17 22:46:56 -0500

    react + vite setup

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import kokoalogo from '../../src/assets/logo-kokoa-100dpi.png'
+
+export const Header = () => {
+  return (
+    <header>
+        <img src={kokoalogo}/>
+    </header>
+  )
+}
